8ab52fa7efa545734718699153bbcc9c7df6b8f2
StonyGround <onekiddo@outlook.com>
2017-06-10 17:31:37 +0800

    v7

---
+++ testAVProcessing/build/intermediates/ndkBuild/debug/obj/local/armeabi/objs-debug/AVProcessing/EncodeAudio.o.d
@@ -0,0 +1,98 @@
+C:/Users/oneki/StudioProjects/video/testAVProcessing/build/intermediates/ndkBuild/debug/obj/local/armeabi/objs-debug/AVProcessing/EncodeAudio.o: \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/EncodeAudio.c \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/AVProcessing.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavcodec/avcodec.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/samplefmt.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/avutil.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/common.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/attributes.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/macros.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/version.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/avconfig.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/mem.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/error.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/rational.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/mathematics.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/intfloat.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/log.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/pixfmt.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/buffer.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/cpu.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/channel_layout.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/dict.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/frame.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavcodec/version.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/avformat.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/avio.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/version.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/opt.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/imgutils.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/pixdesc.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswresample/swresample.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswresample/version.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswscale/swscale.h \
+  C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswscale/version.h
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/AVProcessing.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavcodec/avcodec.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/samplefmt.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/avutil.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/common.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/attributes.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/macros.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/version.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/avconfig.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/mem.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/error.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/rational.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/mathematics.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/intfloat.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/log.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/pixfmt.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/buffer.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/cpu.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/channel_layout.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/dict.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/frame.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavcodec/version.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/avformat.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/avio.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavformat/version.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/opt.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/imgutils.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libavutil/pixdesc.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswresample/swresample.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswresample/version.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswscale/swscale.h:
+
+C:/Users/oneki/StudioProjects/video/testAVProcessing/src/main/jni/include\libswscale/version.h:
--- testAVProcessing/src/main/jni/Android.mk
+++ testAVProcessing/src/main/jni/Android.mk
@@ -1,12 +1,41 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := ffmpeg
-LOCAL_SRC_FILES := prebuilt/libffmpeg.so
+LOCAL_MODULE := avcodec
+LOCAL_SRC_FILES := prebuilt/libavcodec-57.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := avdevice
+LOCAL_SRC_FILES := prebuilt/libavdevice-57.so
+include $(PREBUILT_SHARED_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := avfilter
+LOCAL_SRC_FILES := prebuilt/libavfilter-6.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := avformat
+LOCAL_SRC_FILES := prebuilt/libavformat-57.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := avutil
+LOCAL_SRC_FILES := prebuilt/libavutil-55.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := swresample
+LOCAL_SRC_FILES := prebuilt/libswresample-2.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := swscale
+LOCAL_SRC_FILES := prebuilt/libswscale-4.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := AVProcessing
 LOCAL_SRC_FILES := EncodeAudio.c       \
                    EncodeVideo.c       \
@@ -21,6 +50,5 @@ LOCAL_SRC_FILES := EncodeAudio.c       \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog -ljnigraphics -lz -landroid -lGLESv2
-LOCAL_SHARED_LIBRARIES := ffmpeg
-
+LOCAL_SHARED_LIBRARIES := avcodec avformat avutil swresample swscale
 include $(BUILD_SHARED_LIBRARY)
